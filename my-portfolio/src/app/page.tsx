24c01c883898e0008c50fc9aaedc2225b98e6f03
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-01-09 16:22:24 +0100

    [Add] Component Scroll Panel

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,13 +1,9 @@
+import ScrollPanel from "./components/ScrollPanel";
+
 export default function Home() {
   return (
-      <section className="fixed inset-0 bg-black flex items-center justify-center p-10">
-          <div className="relative bg-red-600 w-full h-full overflow-y-scroll hide-scrollbar">
-              <main>
-                  <h1 className="absolute bottom-4 left-4 text-black font-black text-[20vw] leading-none tracking-tight">
-                      Juan
-                  </h1>
-              </main>
-          </div>
+      <section className="fixed inset-0 bg-black flex items-center justify-center p-7">
+          <ScrollPanel/>
       </section>
   );
 }
