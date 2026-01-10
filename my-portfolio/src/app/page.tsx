3b72562b1830dae10773468f78888c3cc09bd52a
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-01-10 15:23:56 +0100

    [Add] Cursor style

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -2,7 +2,7 @@ import ScrollPanel from "./components/ScrollPanel";
 
 export default function Home() {
   return (
-      <section className="fixed inset-0 bg-black flex items-center justify-center p-7">
+      <section className="fixed inset-0 bg-black flex items-center justify-center p-7 cursor-auto select-none">
           <ScrollPanel/>
       </section>
   );
