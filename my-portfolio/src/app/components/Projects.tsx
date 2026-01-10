2da4fbe0466fa11ff3ebe73b7509559ab120db48
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-01-10 14:39:59 +0100

    [Remove] About-me class and jsx import

--- my-portfolio/src/app/components/Projects.tsx
+++ my-portfolio/src/app/components/Projects.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import {JSX} from "react";
-
 type ProjectsProps ={
     className?: string
 }
 
 export default function Projects({ className }: ProjectsProps) {
     return (
-        <main className={`h-full about-me ${className ?? ""}`}>
+        <main className={`h-full ${className ?? ""}`}>
             <h1 className="absolute bottom-4 left-4 text-black font-black text-[20vw] leading-none tracking-tight">
                 Projects
             </h1>
