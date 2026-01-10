68df556782724c0aaf0f21b1c43e3a1c0396fb49
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-01-10 14:35:51 +0100

    [Create] Status Data

---
+++ my-portfolio/src/data/status.ts
@@ -0,0 +1,16 @@
+import { Status } from "@/types/status";
+
+export const status: Status[] = [
+    {
+        id:1,
+        title: "Coding Now"
+    },
+    {
+        id:2,
+        title: "Running Now"
+    },
+    {
+        id:3,
+        title: "Learning Now"
+    }
+]
