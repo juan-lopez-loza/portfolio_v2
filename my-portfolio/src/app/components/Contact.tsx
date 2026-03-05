a79770b6d793d65fb8d5e944a353f938486889f0
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-03-05 10:21:23 +0100

    [CREATE] contact component

---
+++ my-portfolio/src/app/components/Contact.tsx
@@ -0,0 +1,20 @@
+//
+//  Contact.tsx
+//  Portfolio
+//
+//  Created by Juan Lopez Loza on 27/02/2026.
+//
+
+"use client";
+
+type ContactProps = {
+    className?: string;
+}
+
+export default function Contact({ className }: ContactProps) {
+    return (
+        <main className={`h-full ${className ?? ""}`}>
+
+        </main>
+    );
+}
