777bf292fc3cf712d51db64a0b8204a93a2a5804
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-03-13 23:05:10 +0100

    [ADD] display projects name, url, description

--- my-portfolio/src/app/components/Projects.tsx
+++ my-portfolio/src/app/components/Projects.tsx
@@ -1,15 +1,45 @@
+//
+//  Contact.tsx
+//  Portfolio
+//
+//  Created by Juan Lopez Loza on 27/02/2026.
+//
+
 "use client";
+
+import React from "react";
+import { projects } from "@/data/project"
 
 type ProjectsProps ={
     className?: string
 }
 
 export default function Projects({ className }: ProjectsProps) {
+
+
     return (
         <main className={`h-full ${className ?? ""}`}>
-            <h1 className="absolute bottom-4 left-4 text-black font-black text-[20vw] leading-none tracking-tight">
-                Projects
-            </h1>
+            <div className="absolute top-80 right-8 text-right flex flex-col items-end font-bold text-[3vw] md:text-[1.5vw] uppercase leading-tight z-11">
+                <h3 className="text-yellow-400">
+                    <a href={projects[0].url} target="_blank" rel="noopener noreferrer">
+                        {projects[0].title}
+                    </a>
+                </h3>
+                <span className="text-white whitespace-pre-line">{projects[0].description}</span>
+
+                <h3 className="text-yellow-400">{projects[1].title}</h3>
+                <span className="text-white whitespace-pre-line">{projects[1].description}</span>
+
+                <h3 className="text-yellow-400">{projects[2].title}</h3>
+                <span className="text-white whitespace-pre-line">{projects[2].description}</span>
+
+                <h3 className="text-yellow-400">
+                    <a href={projects[3].url} target="_blank" rel="noopener noreferrer">
+                        {projects[3].title}
+                    </a>
+                </h3>
+                <span className="text-white whitespace-pre-line">{projects[3].description}</span>
+            </div>
         </main>
     );
 }
