e27c5946f5aa5ed35732f42f93271c54e50ef05f
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-04-19 20:14:20 +0200

    [FIX] project data for map

--- my-portfolio/src/data/project.ts
+++ my-portfolio/src/data/project.ts
@@ -1,40 +1,42 @@
 import { Project } from "@/types/project";
 
-export const projects: Project[] = [
-    {
-        id: 1,
-        title: "Site Vitrine Infirmière",
-        url: "https://www.lydgie-cottard.fr",
-        githubUrl: "https://github.com/juan-lopez-loza/nurse_website",
-        description: "Le tout premier site vitrine que j'ai réaliser, dans l'optique de présenter et mettre en avant une infirmière libérale.",
-        technologies: ["HTML", "CSS"],
-        state: "End"
-    },
-    {
-        id: 2,
-        title: "Alt-Tab MacOS",
-        url: null,
-        githubUrl: "https://github.com/juan-lopez-loza/alt_tab",
-        description: "Utilitaire système développé en Swift afin de reproduire le alt-tab window sur mac-os",
-        technologies: ["Swift", "AppKit"],
-        state: "In progress"
-    },
-    {
-        id: 3,
-        title: "To-do liste",
-        url: null,
-        githubUrl: "https://github.com/juan-lopez-loza/E-todo-Epitech",
-        description: "En binôme j'ai réliser une application web de to-do liste avec une gestion utilisateur et un système de partage de to-do liste.",
-        technologies: ["React/Next.js", "Express.js", "Tailwind.css"],
-        state: "End"
-    },
-    {
-        id: 4,
-        title: "Habitly",
-        url: "https://juan-lopez-loza.github.io/Habitly/",
-        githubUrl: "https://juan-lopez-loza.github.io/Habitly/",
-        description: "Application web de gestion d'habitudes avec une gestion utilisateur.",
-        technologies: ["HTML", "CSS", "JS", "Supabase"],
-        state: "In progress"
-    }
-];
+export const projects = {
+    project: [
+        {
+            id: 1,
+            title: "Site Vitrine Infirmière",
+            url: "https://www.lydgie-cottard.fr",
+            githubUrl: "https://github.com/juan-lopez-loza/nurse_website",
+            description: "Le tout premier site vitrine que j'ai réaliser, dans l'optique de présenter et mettre en avant une infirmière libérale.",
+            technologies: ["HTML", "CSS"],
+            state: "End"
+        },
+        {
+            id: 2,
+            title: "Alt-Tab MacOS",
+            url: null,
+            githubUrl: "https://github.com/juan-lopez-loza/alt_tab",
+            description: "Utilitaire système développé en Swift afin de reproduire le alt-tab window sur mac-os",
+            technologies: ["Swift", "AppKit"],
+            state: "In progress"
+        },
+        {
+            id: 3,
+            title: "To-do liste",
+            url: null,
+            githubUrl: "https://github.com/juan-lopez-loza/E-todo-Epitech",
+            description: "En binôme j'ai réliser une application web de to-do liste avec une gestion utilisateur et un système de partage de to-do liste.",
+            technologies: ["React/Next.js", "Express.js", "Tailwind.css"],
+            state: "End"
+        },
+        {
+            id: 4,
+            title: "Habitly",
+            url: "https://juan-lopez-loza.github.io/Habitly/",
+            githubUrl: "https://juan-lopez-loza.github.io/Habitly/",
+            description: "Application web de gestion d'habitudes avec une gestion utilisateur.",
+            technologies: ["HTML", "CSS", "JS", "Supabase"],
+            state: "In progress"
+        }
+    ]
+}
