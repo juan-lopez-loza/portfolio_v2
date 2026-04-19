4af62390b86105b4bbcfa7dd829d31db3cb8443b
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-04-19 19:42:47 +0200

    [ADD] repositories url

--- my-portfolio/src/data/project.ts
+++ my-portfolio/src/data/project.ts
@@ -4,7 +4,8 @@ export const projects: Project[] = [
     {
         id: 1,
         title: "Site Vitrine Infirmière",
-        url: "https://juan-lopez-loza.github.io/nurse_website/",
+        url: "https://www.lydgie-cottard.fr",
+        githubUrl: "https://github.com/juan-lopez-loza/nurse_website",
         description: "Le tout premier site vitrine que j'ai réaliser, dans l'optique de présenter et mettre en avant une infirmière libérale.",
         technologies: ["HTML", "CSS"],
         state: "End"
@@ -13,6 +14,7 @@ export const projects: Project[] = [
         id: 2,
         title: "Alt-Tab MacOS",
         url: null,
+        githubUrl: "https://github.com/juan-lopez-loza/alt_tab",
         description: "Utilitaire système développé en Swift afin de reproduire le alt-tab window sur mac-os",
         technologies: ["Swift", "AppKit"],
         state: "In progress"
@@ -21,6 +23,7 @@ export const projects: Project[] = [
         id: 3,
         title: "To-do liste",
         url: null,
+        githubUrl: "https://github.com/juan-lopez-loza/E-todo-Epitech",
         description: "En binôme j'ai réliser une application web de to-do liste avec une gestion utilisateur et un système de partage de to-do liste.",
         technologies: ["React/Next.js", "Express.js", "Tailwind.css"],
         state: "End"
@@ -29,6 +32,7 @@ export const projects: Project[] = [
         id: 4,
         title: "Habitly",
         url: "https://juan-lopez-loza.github.io/Habitly/",
+        githubUrl: "https://juan-lopez-loza.github.io/Habitly/",
         description: "Application web de gestion d'habitudes avec une gestion utilisateur.",
         technologies: ["HTML", "CSS", "JS", "Supabase"],
         state: "In progress"
--- my-portfolio/src/types/project.ts
+++ my-portfolio/src/types/project.ts
@@ -2,8 +2,8 @@ export type Project = {
     id: number;
     title: string;
     url: string | null;
+    githubUrl: string;
     description: string;
     technologies: string[];
     state: "End" | "In progress";
-    link?: string;
 };
