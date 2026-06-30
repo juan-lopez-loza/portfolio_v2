fafe095c29d773d3ff8004cf1061c54311ad349e
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-30 11:40:21 +0200

    [+] project eliza

--- my-portfolio/src/data/project.ts
+++ my-portfolio/src/data/project.ts
@@ -13,6 +13,24 @@ export const projects = {
         },
         {
             id: 2,
+            title: "Habitly",
+            url: "https://juan-lopez-loza.github.io/Habitly/",
+            githubUrl: "https://juan-lopez-loza.github.io/Habitly/",
+            description: "Application web de gestion d'habitudes avec une gestion utilisateur.",
+            technologies: ["HTML", "CSS", "JS", "Supabase"],
+            state: "In progress"
+        },
+        {
+            id: 3,
+            title: "Petzl Ai Chatbot",
+            url: null,
+            githubUrl: "https://github.com/juan-lopez-loza/Eliza_Epitech",
+            description: "En binôme, nous avons réalisé un chatbot expert en excalade disponible sur discord et slack mais aussi sur une landig page.",
+            technologies: ["Python", "JS", "React", "Tailwind.css", "LM Studio"],
+            state: "End"
+        },
+        {
+            id: 4,
             title: "Alt-Tab MacOS",
             url: null,
             githubUrl: "https://github.com/juan-lopez-loza/alt_tab",
@@ -21,22 +39,13 @@ export const projects = {
             state: "In progress"
         },
         {
-            id: 3,
+            id: 5,
             title: "To-do liste",
             url: null,
             githubUrl: "https://github.com/juan-lopez-loza/E-todo-Epitech",
             description: "En binôme j'ai réliser une application web de to-do liste avec une gestion utilisateur et un système de partage de to-do liste.",
             technologies: ["React/Next.js", "Express.js", "Tailwind.css"],
             state: "End"
-        },
-        {
-            id: 4,
-            title: "Habitly",
-            url: "https://juan-lopez-loza.github.io/Habitly/",
-            githubUrl: "https://juan-lopez-loza.github.io/Habitly/",
-            description: "Application web de gestion d'habitudes avec une gestion utilisateur.",
-            technologies: ["HTML", "CSS", "JS", "Supabase"],
-            state: "In progress"
         }
     ]
 }
